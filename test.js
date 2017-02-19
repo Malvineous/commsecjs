f82a167ad7fa9c87d626d4237e3f95b93e36fde7
Adam Nielsen <malvineous@shikadi.net>
2017-02-19 13:09:34 +1000

    Add test code for retrieving confirmations

--- test.js
+++ test.js
@@ -39,3 +39,17 @@ b.fill_prices(stocks).then(function(s) {
 		console.log('Order failed: ' + order.error);
 	});
 });
+
+// cs_connect_if_needed() isn't really needed (and will cause two parallel
+// connection attempts because fill_prices() above will also try to connect)
+// so probably best to avoid it, but it's here to serve as an example of how
+// it should be used, if you are inclined to use it at the start of your code
+// so you can more accurately detect login failures.
+b.cs_connect_if_needed().then(function() {
+	b.cs_get_confirmations_range(new Date('2016-01-01'), new Date('2016-01-31')).then(function(list) {
+		console.log('Getting confirmations succeeded:');
+		console.log(list);
+	}, function(err) {
+		console.log('Getting confirmations failed: ' + err);
+	});
+});
